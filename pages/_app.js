d2ca95c9b6754f4f6877764fdeae9064905d60f3
Bhavya <xbhavyatomar@gmail.com>
2021-04-30 03:32:10 +0530

    use express server

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import '../styles/globals.css'
-
-
+import "../styles/globals.css";
+import { v4 } from "uuid";
+import { useState } from "react";
+import { SocketProvider } from "../context/SocketProvider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [id, setId] = useState(v4());
+  return (
+    <SocketProvider id={id}>
+      <Component {...pageProps} />
+    </SocketProvider>
+  );
 }
-{/* <Editor
+{
+  /* <Editor
   apiKey="69ghvwqw564y6c5ccbxq3w4jdqxp6smlup8xse4dmvn3kvev"
   value={this.usestate.Content}
   init={{
@@ -13,5 +20,6 @@ function MyApp({ Component, pageProps }) {
     menubar: false
   }}
   onEditorChange={this.handleChange}
-/> */}
-export default MyApp
+/> */
+}
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 import UsernameField from "../components/UsernameField";
-import 'emoji-mart/css/emoji-mart.css';
-import { Picker } from 'emoji-mart';
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
+import { useSocket } from "../context/SocketProvider";
 
 // import { Editor } from "@tinymce/tinymce-react";
 
-
-
-
-
-  
 export default function Home() {
   // save the socket
-  const [socket, setSocket] = useState(null);
+  // const [socket, setSocket] = useState(null);
+  const socket = useSocket();
 
   // Whether the username is set.
   const [isUsernameConfirmed, setUsernameConfirmed] = useState(false);
@@ -36,38 +33,59 @@ export default function Home() {
     */
   ]);
 
-  const connectSocket = () => {
-    // prime the server first. yes, this is an extra call and is inefficient.
-    // but we're using NextJS for convenience, so this is a necessary evil.
-    fetch("/api/chat");
-    // after making sure that socket server is primed, connect to it.
+  // const connectSocket = () => {
+  //   // prime the server first. yes, this is an extra call and is inefficient.
+  //   // but we're using NextJS for convenience, so this is a necessary evil.
+  //   fetch("/api/chat");
+  //   // after making sure that socket server is primed, connect to it.
 
-    if (!socket) {
-      const newSocket = io();
+  // if (!socket) {
+  //     const newSocket = io();
 
-      // Confirms connection
-      newSocket.on("connect", () => {
-        console.log("Chat app connected");
-      });
-
-      // handles message
-      newSocket.on("message", (msg) => {
-        setHistory((history) => [...history, msg]);
-      });
-
-      // Logs when server disconnects
-      newSocket.on("disconnect", () => {
-        console.warn("WARNING: chat app disconnected");
-      });
-
-      setSocket(() => newSocket);
-    }
-  };
-
-  // The websocket code
+  //     // Confirms connection
   useEffect(() => {
-    connectSocket();
-  }, []);
+    if (socket == null) return;
+
+    socket.on("connect", () => {
+      console.log("Chat app connected");
+    });
+
+    //     // handles message
+    socket.on("message", (msg) => {
+      console.log("message");
+      setHistory((history) => [...history, msg]);
+    });
+
+    //     // Logs when server disconnects
+    socket.on("disconnect", () => {
+      console.warn("WARNING: chat app disconnected");
+    });
+  }, [socket]);
+  // if (socket) {
+  //   socket.on("connect", () => {
+  //     console.log("Chat app connected");
+  //   });
+
+  //   //     // handles message
+  //   socket.on("message", (msg) => {
+  //     console.log("message");
+  //     setHistory((history) => [...history, msg]);
+  //   });
+
+  //   //     // Logs when server disconnects
+  //   socket.on("disconnect", () => {
+  //     console.warn("WARNING: chat app disconnected");
+  //   });
+  // }
+
+  //     setSocket(() => newSocket);
+  //   }
+  // };
+
+  // // The websocket code
+  // useEffect(() => {
+  //   connectSocket();
+  // }, []);
 
   // this method submits the form and sends the message to the server.
   const handleSubmit = (evt) => {
@@ -87,95 +105,88 @@ export default function Home() {
     setMessage("");
   };
 
-
   // handleChange(content, editor); {
   //   this.setState({content});
   // }
-//  const websocket=require("ws") 
-//   const rooms = {};
+  //  const websocket=require("ws")
+  //   const rooms = {};
 
-//   ws.on("connection", socket => {
-//     const uuid = uuid // create here a uuid for this connection
-  
-//     const leave = room => {
-//       // not present: do nothing
-//       if(! rooms[room][uuid]) return;
-  
-//       // if the one exiting is the last one, destroy the room
-//       if(Object.keys(rooms[room]).length === 1) delete rooms[room];
-//       // otherwise simply leave the room
-//       else delete rooms[room][uuid];
-//     };
-  
-//     socket.on("message", data => {
-//       const { message, meta, room } = data;
-  
-//       if(meta === "join") {
-//         if(! rooms[room]) rooms[room] = {}; // create the room
-//         if(! rooms[room][uuid]) rooms[room][uuid] = socket; // join the room
-//       }
-//       else if(meta === "leave") {
-//         leave(room);
-//       }
-//       else if(! meta) {
-//         // send the message to all in the room
-//         Object.entries(rooms[room]).forEach(([, sock]) => sock.send({ message }));
-//       }
-//     });
-  
-//     socket.on("close", () => {
-//       // for each room, remove the closed socket
-//       Object.keys(rooms).forEach(room => leave(room));
-//     });
-//   });
+  //   ws.on("connection", socket => {
+  //     const uuid = uuid // create here a uuid for this connection
 
+  //     const leave = room => {
+  //       // not present: do nothing
+  //       if(! rooms[room][uuid]) return;
 
+  //       // if the one exiting is the last one, destroy the room
+  //       if(Object.keys(rooms[room]).length === 1) delete rooms[room];
+  //       // otherwise simply leave the room
+  //       else delete rooms[room][uuid];
+  //     };
 
+  //     socket.on("message", data => {
+  //       const { message, meta, room } = data;
+
+  //       if(meta === "join") {
+  //         if(! rooms[room]) rooms[room] = {}; // create the room
+  //         if(! rooms[room][uuid]) rooms[room][uuid] = socket; // join the room
+  //       }
+  //       else if(meta === "leave") {
+  //         leave(room);
+  //       }
+  //       else if(! meta) {
+  //         // send the message to all in the room
+  //         Object.entries(rooms[room]).forEach(([, sock]) => sock.send({ message }));
+  //       }
+  //     });
+
+  //     socket.on("close", () => {
+  //       // for each room, remove the closed socket
+  //       Object.keys(rooms).forEach(room => leave(room));
+  //     });
+  //   });
 
   return (
-    
-   <div className={styles.chat_window}>
+    <div className={styles.chat_window}>
       <Head>
-      
-        <title >chit-chat</title>
-    
-
+        <title>chit-chat</title>
       </Head>
 
-      
       {/* <audio className={styles.audio} src="/Users/tanyaprabhakar/Downloads/Mahmut Orhan - Game Of Thrones (Original Mix).mp3" loop="true" autostart="true" width="2" height="0"></embed></audio> */}
-      
-      
-      <h1 className={styles.top_menu}className={styles.text}> Game of thrones </h1>
 
-      <div className={styles.alignment} >
-      <UsernameField className={styles.second_bar}
-        completed={isUsernameConfirmed}
-        value={username}
-        onChange={(value) => setUsername(value)}
-        onSubmit={() => setUsernameConfirmed(true)}
-      /></div>
-{/*   <ul class="messages"></ul>
+      <h1 className={styles.top_menu} className={styles.text}>
+        {" "}
+        Game of thrones{" "}
+      </h1>
+
+      <div className={styles.alignment}>
+        <UsernameField
+          className={styles.second_bar}
+          completed={isUsernameConfirmed}
+          value={username}
+          onChange={(value) => setUsername(value)}
+          onSubmit={() => setUsernameConfirmed(true)}
+        />
+      </div>
+      {/*   <ul class="messages"></ul>
     <div class="bottom_wrapper clearfix">*/}
 
-      <div  className={styles.message_container}>
-        {history.map(({ username, message }, i)  => (
-          <div className={styles.chatbox} key={i}> 
+      <div className={styles.message_container}>
+        {history.map(({ username, message }, i) => (
+          <div className={styles.chatbox} key={i}>
             <p> </p>
-            <p><b>{username}</b>: {message}
-            </p></div>
+            <p>
+              <b>{username}</b>: {message}
+            </p>
+          </div>
         ))}
-        </div>
-      
-        <div className={styles.alignment}>
-        <div className={styles.bottom_wrapper .message_input_wrapper}>
+      </div>
+
+      <div className={styles.alignment}>
+        <div className={styles.bottom_wrapper.message_input_wrapper}>
           <main className={styles.bottom_wrapper}>
-          
-          
-          
-          
-          <form  onSubmit={handleSubmit} >
-          {/* <Editor
+            <form onSubmit={handleSubmit}>
+              {/* <Editor
       value={this.useState.content}
       init={{
       height: 500,
@@ -185,30 +196,35 @@ export default function Home() {
       />
     <br />
            */}
-      
-    
-          <label >
-            
-            <input className={styles.message_input}
-              id="textarea"
-              type="text"
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder={
-                username ? "Enter your message..." : "Set username..."
-              }
-              disabled={!isUsernameConfirmed}
-            />
-          </label>
-          <input className={styles.submit}type="submit" value="Submit" disabled={!isUsernameConfirmed} />
-        </form></main>
-        </div></div>
 
+              <label>
+                <input
+                  className={styles.message_input}
+                  id="textarea"
+                  type="text"
+                  name="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder={
+                    username ? "Enter your message..." : "Set username..."
+                  }
+                  disabled={!isUsernameConfirmed}
+                />
+              </label>
+              <input
+                className={styles.submit}
+                type="submit"
+                value="Submit"
+                disabled={!isUsernameConfirmed}
+              />
+            </form>
+          </main>
+        </div>
+      </div>
 
-      
       {/* <footer className={styles.footera}>
         
       </footer> */}
     </div>
-  )}
+  );
+}
