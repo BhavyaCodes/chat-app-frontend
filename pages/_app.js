d138cc68733b83f914fca1e113e387f0301e37fc
tanyaapb <72110047+tanyaapb@users.noreply.github.com>
2021-04-28 18:07:35 -0700

    adding next directory

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
+{/* <Editor
+  apiKey="69ghvwqw564y6c5ccbxq3w4jdqxp6smlup8xse4dmvn3kvev"
+  value={this.usestate.Content}
+  init={{
+    height: 500,
+    menubar: false
+  }}
+  onEditorChange={this.handleChange}
+/> */}
 export default MyApp
